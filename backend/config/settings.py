f983c68211541dbae9ce7cd52f64f98b771de04f
umarazlan <umarazlan5787@gmail.com>
2026-08-28 18:40:40 +0500

    Fix production database configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -81,7 +81,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
 
@@ -132,8 +133,8 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    # Add your Render frontend URL here after deployment
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+#     # Add your Render frontend URL here after deployment
+# ]
